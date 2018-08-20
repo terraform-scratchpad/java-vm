b01c4d52968495618a0572d5561496219f79bcac
Abderrazak BOUADMA <bouadma.abderrazak@gmail.com>
2018-08-21 00:26:31 +0200

    fixed vars naming

--- dior-com/outputs.tf
+++ dior-com/outputs.tf
@@ -7,13 +7,13 @@
 # VM Outputs
 #
 output "vm-public-ip" {
-  value = "${module.vm-01.vm-public-ip}"
+  value = "${module.vm.vm-public-ip}"
 }
 
 output "vm-admin-username" {
-  value = "${module.vm-01.vm-admin-username}"
+  value = "${module.vm.vm-admin-username}"
 }
 
 output "vm-admin-password" {
-  value = "${module.vm-01.vm-admin-password}"
+  value = "${module.vm.vm-admin-password}"
 }
